0495db26b38b465e718f71f381f612170f91fbf3
olmedix <olmedix@gmail.com>
2023-12-18 17:24:25 +0100

    comento ejer 18

--- Tema3/src/cadenas/Ejercicio18.java
+++ Tema3/src/cadenas/Ejercicio18.java
@@ -15,10 +15,17 @@ public class Ejercicio18 {
 		
 		while(fin != -1){
 			
+			// con UpperCase ponemos mayúscula y con charAT 
+			// le decimos que  tiene que ser la primera letra
 			System.out.print(frase.toUpperCase().charAt(inicio));
+
+			// A continuación le pedimos que escriba a partir desde inicio +1 hasta 
+			// el siguiente espacio. 
 			System.out.print(frase.substring(inicio+1,fin) + " "); 
 			inicio=fin+1;
 			
+			// Actualizamos el fin de palabra. Hasta que devuelva -1
+			// para salir del bucle.
 			fin = frase.indexOf(' ',inicio);
 			
 			

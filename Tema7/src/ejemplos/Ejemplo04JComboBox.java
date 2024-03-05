c99d3b37224d5b8ba3648f8f745d5368daf762a9
olmedix <olmedix@gmail.com>
2024-03-05 20:11:02 +0100

    clase 5 de marzo

--- Tema7/src/ejemplos/Ejemplo04JComboBox.java
+++ Tema7/src/ejemplos/Ejemplo04JComboBox.java
@@ -1,10 +1,17 @@
 package ejemplos;
 
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Ejemplo04JComboBox extends JFrame {
 
@@ -13,7 +20,25 @@ public class Ejemplo04JComboBox extends JFrame {
 		super("Províncias");
 		setSize(400, 300);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setLayout(new FlowLayout());
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		try {
+			// Aplicamos el tema (look and feel) llamado Nimbus
+			 UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+
+			// Aplicamos el tema (look and feel) del sistema operativo.
+			// UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+			//UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+
+			// Esta instrucción asegura que el tema se aplique correctamente a toda la
+			// interfaz de usuario.
+			SwingUtilities.updateComponentTreeUI(this);
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+
+			System.out.println("Error al cambiar el Look and Feel");
+		}
 
 		JLabel lblProvincia = new JLabel("Seleccion tu provincia preferida:");
 		JComboBox<String> cmbProvincias = new JComboBox<>();
@@ -24,8 +49,30 @@ public class Ejemplo04JComboBox extends JFrame {
 		cmbProvincias.addItem("Jaén");
 		cmbProvincias.addItem("Albacete");
 
+		// para pasar un tamanyo al desplegable, setSize no funciona para esto.
+		cmbProvincias.setPreferredSize(new Dimension(300, 20));
+
+		JLabel lblDescripcion = new JLabel("¿Por qué es tu pronvincia favorita?");
+
+		JTextArea txtDescripcion = new JTextArea();
+
+		// para que no genere scroll horizontal.
+		txtDescripcion.setLineWrap(true);
+
+		// para que no corte las palabras.
+		txtDescripcion.setWrapStyleWord(true);
+
+		JScrollPane scrPanel = new JScrollPane(txtDescripcion);
+		scrPanel.setPreferredSize(new Dimension(300, 80));
+
+		JButton btnGuardar = new JButton("Guardar");
+		btnGuardar.setPreferredSize(new Dimension(130, 25));
+
 		add(lblProvincia);
 		add(cmbProvincias);
+		add(lblDescripcion);
+		add(scrPanel);
+		add(btnGuardar);
 
 		setVisible(true);
 
---
+++ Tema7/src/ejemplos/Ejemplo05DetectarLook.java
@@ -0,0 +1,18 @@
+package ejemplos;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+public class Ejemplo05DetectarLook extends JFrame {
+	public Ejemplo05DetectarLook() {
+		UIManager.LookAndFeelInfo[] laf = UIManager.getInstalledLookAndFeels();
+		for (int i = 0; i < laf.length; i++) {
+			System.out.println("Class name: " + laf[i].getClassName());
+			System.out.println("Name: " + laf[i].getName() + "\n");
+		}
+	}
+
+	public static void main(String[] arguments) {
+		Ejemplo05DetectarLook dialog = new Ejemplo05DetectarLook();
+	}
+}

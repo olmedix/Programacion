7cf3ee9fb2f5b69053e65d1224ba62ccf8ee999f
olmedix <olmedix@gmail.com>
2024-05-10 17:53:50 +0200

    falta el 6

--- Tema9/src/ejerciciosRecursividad/Ejercicio05.java
+++ Tema9/src/ejerciciosRecursividad/Ejercicio05.java
@@ -2,28 +2,24 @@ package ejerciciosRecursividad;
 
 public class Ejercicio05 {
 
+
 	public static void main(String[] args) {
-		
-		// Escribe un método que calcule recursivamente la suma de todos los números enteros entre 1 y n.
-		
-		System.out.println(sumaEnteros(3));
+
+		// Escribe un método que calcule recursivamente la suma de todos los números
+		// enteros entre 1 y n.
+
+		System.out.println("El numero es " + sumaEnteros(3));
 
 	}
-	
+
 	private static int sumaEnteros(int n) {
-		
-		int contador=1;
-		
-		if(contador > n) {
-			System.out.println("hola");
+
+		if (1 <= n) {
+
+			return sumaEnteros(n-1) +  n;
+		} else {
 			return 0;
 		}
-		
-		
-		System.out.println(contador);
-		
-		return sumaEnteros(contador+(contador+1));
-		
 	}
 
 }
